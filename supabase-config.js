fb14fc97571c1d17487b1bde306ff633b9f3b731
carbar1000 <carlosbarradas.cripto@gmail.com>
2025-02-18 17:10:41 +0000

    enviando para o giti

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 const supabaseUrl = window.SUPABASE_URL;
 const supabaseKey = window.SUPABASE_ANON_KEY;
 
-const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
+window.supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
